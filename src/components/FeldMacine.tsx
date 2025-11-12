ea57d1d9f840c6dff5abd53c6c79bfa99b2541f2
Gnomus111 <name.sure2000@mail.ru>
2025-11-12 13:37:19 +0300

    ...

--- src/components/FeldMacine.tsx
+++ src/components/FeldMacine.tsx
@@ -1,6 +1,3 @@
-
-
-
 import { TextField } from "@mui/material";
 
  interface FeldMacineProps {
--- src/pages/VendingMachine.tsx
+++ src/pages/VendingMachine.tsx
@@ -2,15 +2,13 @@ import React from "react"
 import NumbMachne from "../components/NumbMachne"
 
 
-import { Box, Typography } from "@mui/material"
+import { Box } from "@mui/material"
 import FeldMacine from "../components/FeldMacine"
 
 
 
 
 const VendingMachine = () => {
-
-
     const [Watchcounter, setWatchcounter] = React.useState(0);
 
     const counter = (value: number) => {
